f6d0f857b2da79f7f3031322abb12c70792d3296
bryant1990 <bryant1990@mail.ru>
2018-06-02 19:48:59 +0400

    FirstBasicTryWithTest

---
+++ IOC/BankAccountTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BankAccountTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BankAccountTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a20d8d9e-2c6c-4ff1-a241-91aff76ada2a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ IOC/IOC/BankAccount.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace BankAccountNS
+{
+    public class BankAccount
+    {
+        private string m_customerName;
+
+        private double m_balance;
+
+        private bool m_frozen = false;
+
+        private BankAccount()
+        {
+        }
+
+        public BankAccount(string customerName, double balance)
+        {
+            m_customerName = customerName;
+            m_balance = balance;
+        }
+
+        public string CustomerName
+        {
+            get { return m_customerName; }
+        }
+
+        public double Balance
+        {
+            get { return m_balance; }
+        }
+
+        public void Debit(double amount)
+        {
+            if (m_frozen)
+            {
+                throw new Exception("Аккаунт замарожен");
+            }
+
+            if (amount > m_balance)
+            {
+                throw new ArgumentOutOfRangeException("Некорректное значение amount");
+            }
+
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("Некорректное значение  amount");
+            }
+
+            m_balance -= amount;
+        }
+
+        private void FreezeAccount()
+        {
+            m_frozen = true;
+        }
+
+        private void UnfreezeAccount()
+        {
+            m_frozen = false;
+        }
+
+        public static void Main()
+        {
+            BankAccount ba = new BankAccount("Сибгатуллов", 11.99);
+            ba.Debit(11.22);
+            Console.WriteLine("Текущий баланс ${0}", ba.Balance);
+        }
+
+    }
+}
+
