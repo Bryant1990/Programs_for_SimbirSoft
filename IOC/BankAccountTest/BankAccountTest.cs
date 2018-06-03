29317450f79e61e3f93b9b586d5b2ed3a5c83780
bryant1990 <bryant1990@mail.ru>
2018-06-03 18:26:31 +0400

    3 Tests

---
+++ IOC/BankAccountTest/BankAccountTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankAccountNS;
+
+namespace BankTest
+{
+    [TestClass]
+    public class BankAccountTests
+    {
+        [TestMethod]
+        public void Debit_WithValidAmount_UpdatesBalance()
+        {
+            //arrange 
+            double beginningBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            BankAccount account = new BankAccount("Петров", beginningBalance);
+
+            // act 
+            account.Debit(debitAmount);
+
+            // assert 
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Тест прошел корректно");
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Debit_WhenAmountIsLessThanZero_ShouldThrowArgumentOutOfRange()
+        {
+            // arrange
+            double beginningBalance = 11.99;
+            double debitAmount = -100.00;
+            BankAccount account = new BankAccount("Сидоров", beginningBalance);
+
+            // act
+            account.Debit(debitAmount);   
+            
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Debit_WhenAmountIsMoreThanBalance_ShouldThrowArgumentOutOfRange()
+        {
+            // arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 15.99;
+            BankAccount account = new BankAccount("Михайлов", beginningBalance);
+
+            // act
+            account.Debit(debitAmount);            
+        }
+    }
+}
