eee203a04b707ac0d039a5297574fab7c18e27b8
bryant1990 <bryant1990@mail.ru>
2018-06-17 14:59:04 +0400

    3 classes

---
+++ Programs_for_SimbirSoft-Selenium/OpenAvitoWithSeleniumToFindCats/OpenAvitoWithSeleniumToFindCats/MainPage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+
+namespace OpenAvitoWithSeleniumToFindCats
+{
+    class MainPage
+    {
+        ChromeDriver Chrome;
+        By RefPats = By.LinkText("Животные");        
+        public MainPage(ChromeDriver Chrome)
+        {
+            this.Chrome = Chrome;
+        }
+        public void ClickPats()
+        {
+            Chrome.FindElement(RefPats).Click();
+        }
+    }
+}
---
+++ Programs_for_SimbirSoft-Selenium/OpenAvitoWithSeleniumToFindCats/OpenAvitoWithSeleniumToFindCats/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace OpenAvitoWithSeleniumToFindCats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WorkWithChrome();
+        }
+
+        static void WorkWithChrome ()
+        {
+            ChromeDriver Chrome;            
+            Chrome = new ChromeDriver(@"C:\Users\Компьютер\Downloads\chromedriver_win32");
+            //Chrome = new ChromeDriver();
+            Chrome.Manage().Window.Maximize();
+            Chrome.Navigate().GoToUrl("https://www.avito.ru/ulyanovsk");
+            MainPage mainPage = new MainPage(Chrome);
+            mainPage.ClickPats();
+            Pats pats = new Pats(Chrome);
+            pats.ClickCats();
+            Cats cats = new Cats(Chrome);
+            cats.MakeList();
+            cats.FindCatWithMaxOrder();
+            cats.ShowCats();                        
+            Chrome.Quit();
+            Console.ReadLine();
+        }        
+    }
+}
